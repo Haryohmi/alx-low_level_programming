643eb27538d37d76973ac24183415b774a9bddc1
Mayowa <mahyorfrancis@gmail.com>
2022-06-30 16:22:51 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-d./**
+/**
  * main - this is a program that prints a message
  * Return: returns 0 if message prints successfully
  */
 #include <stdio.h>
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art, \n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
