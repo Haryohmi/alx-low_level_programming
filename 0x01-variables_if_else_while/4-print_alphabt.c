5ae0a68d944146b0c903da17e51516e00ccfe5ef
Mayowa <mahyorfrancis@gmail.com>
2022-07-03 10:32:16 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-printf("%d", num);
-printf("\n");
-return (0);
+  char letter;
+  for (letter = 'a'; letter <= 'z'; letter++)
+    putchar(letter);
+  putchar('\n');
+  return (0);
 }
