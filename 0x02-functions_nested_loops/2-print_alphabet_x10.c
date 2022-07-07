8ffc90484c0dbcb4b217ac9f42b618ded4602a78
Mayowa <mahyorfrancis@gmail.com>
2022-07-07 09:29:52 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * main - a function that prints 10 times the
+ * print_alphabet_x10 - function to print abc 10
  *
- * alphabet, in lowercase, followed by a new line.
+ * times
  *
  * Return: 0
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
   char c, i;
-  
-  for (i = 0; i <= 9, i++)
-    {
-      for (c = 'a'; c <= 'z'; c++)
-	{
-	  _putchar(c);
-	}
-      _putchar('\n');:
-    }
+     for (i = 0; i <= 9; i++)
+       {
+	 for (c = 'a'; c <= 'z'; c++)
+	   {
+	     _putchar(c);
+	   }
+	 _putchar('\n');:
+       }
 }
