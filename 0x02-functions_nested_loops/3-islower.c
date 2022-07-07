e86fa3063a1f118738b58edce833fd26116793c4
Mayowa <mahyorfrancis@gmail.com>
2022-07-07 09:48:54 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - function to check for lowercase
  *
- * character c is the int that will use for the
+ * character @c: is the int that will use for the
  *
  * argument of the function
  *
