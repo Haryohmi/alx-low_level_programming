42d5f025a28b6730872e46660bf68f50a742e127
Mayowa <mahyorfrancis@gmail.com>
2022-07-07 16:28:58 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,24 +2,24 @@
 
 /**
  * print_sign - prints the sign of a number
- *@n: number being tested
+ * @n: number being tested
  * Return: Always 0.
  */
 int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('+');
+_putchar(43);
 return (1);
 }
 else if (n == 0)
-}
-_putchar('0');
-return (0);
-}
-else if (n < 0)
 {
-_putchar('-');
-return (-1);
+_putchar(48);
 return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
 }
