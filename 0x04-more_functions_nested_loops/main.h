4a4ff76ebb2e2b2a78902119d7b98323cac436b5
Mayowa <mahyorfrancis@gmail.com>
2022-07-07 18:24:34 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,14 @@
+#ifndef _MAIN.H_
+#define _MAIN.H_
 void _putchar(char);
 void print_numbers(void);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+#endf
